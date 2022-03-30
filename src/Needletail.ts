130ed63186a66aec790a02437ef3cf00d02a2d1f
Edsardio <e.k.j.folkers@live.nl>
2022-03-30 16:04:36 +0200

    export config

--- src/Needletail.ts
+++ src/Needletail.ts
@@ -2,6 +2,7 @@
  * Export all main components
  */
 export * from './Client';
+export * from './Helpers/Config';
 export * from './Models/Filters';
 export * from './Models/Groupers';
 export * from './Models/Queries';
